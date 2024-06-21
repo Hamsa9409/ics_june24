b78222d2620c94bd83fbe7656b218f585419de45
Hamsa Ramesh <hamsar@infinite.com>
2024-06-21 06:49:19 +0530

    updated assignment

--- c sharp/assignment/assignment_2/assignment_2/array assignment.cs
+++ c sharp/assignment/assignment_2/assignment_2/array assignment.cs
@@ -15,52 +15,51 @@ namespace assignment_2
             for (int i=0;i<marks.Length;i++)
             {
                 Console.WriteLine($" enter marks {i + 1}:");
-                Console.ReadLine();
+                marks[i] = int.Parse(Console.ReadLine());
 
             }
+            
 
             markscalculation(marks, out int total, out double average, out int minvalue, out int maxvalue/*, out int[] ascendingorder, out int[] descendingorder*/);
 
-            Console.WriteLine("total marks is" + total + "average is" + average + "minimum value is" + minvalue + "maximum value is" + maxvalue);
-            Console.WriteLine("total marks is" + total);
-            Console.WriteLine("average is" + average);
-            Console.WriteLine("minimum value is" + minvalue);
-            Console.WriteLine("maximum value is" + maxvalue);
+            Console.WriteLine("total marks is : "  +  total  +  "average is : "  +  average  +  "minimum value is :"  +  minvalue  +  "maximum value is :"  +  maxvalue);
+            
             Console.Read();
 
 
 
         }
-        static void markscalculation(int[] marks,out int total,out double average, out int minvalue,out int maxvalue/*,out int[] ascendingorder,out int[] descendingorder*/)
+        static void markscalculation(int[] marks, out int total, out double average, out int minvalue, out int maxvalue/*,out int[] ascendingorder,out int[] descendingorder*/)
         {
             total = 0;
-            foreach(int mark in marks)
+            foreach (int mark in marks)
             {
                 total += mark;
             }
-            
 
-            average =(double) total / marks.Length;
-            
+
+            average = (double)total / marks.Length;
+
 
 
             minvalue = marks[0];
             maxvalue = marks[0];
-            for(int i=1;i<marks.Length;i++)
+            for (int i = 1; i < marks.Length; i++)
             {
                 if (marks[i] < minvalue)
                 {
                     minvalue = marks[i];
 
                 }
-                else if((marks[i] > maxvalue))
+                else if ((marks[i] > maxvalue))
                 {
                     maxvalue = marks[i];
 
                 }
             }
-           
-        }
-        
+
+
+
+        }   
     }
 }
